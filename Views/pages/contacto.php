5356e04311387542c86b50779d2ccabcb3323370
n9bes <144210392+n9bes@users.noreply.github.com>
2024-04-10 16:23:36 -0300

    Add files via upload

---
+++ Views/pages/contacto.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="../img/facebook.png" alt="facebook logo" width="40">
+            <img src="../img/instagram.png" alt="instagram logo" width="40"> <br>
+            FREE SHIPPING AFTER 50$ <br>
+            <img src="../img/us.png" alt="usFlag" width="20">
+        </div>
+        <div>
+            <h2>LOGO</h2>
+            <form action="/search" method="get">
+                <input type="text" name="search" placeholder="What are you searching for?">
+                <button type="submit" class="search-button">
+                    <img src="../img/magnifying-glass.png" alt="magnifying-glass logo" width="40">
+                </button>
+            </form>
+        </div>
+        <div>
+            <ul>
+                <img src="../img/bubble-chat.png" alt="Chat Logo" width="100">Ayuda<img/>
+                <li><img src="../img/whatsapp.png" alt="whatsapp image" width="100"><a href="https://www.whatsapp.com">+54 9999 99-9999</a></img></li>
+                <li><img src="../img/email.png" alt="email logo" width="100">example@gmail.com</li>
+            </ul>
+        </div>
+        <div>
+            <img src="../img/user.png" alt="user logo" width="100"><img/>
+            <br>
+            Mi perfil
+            <ul>
+                <li><a href="CreateAcc.html">Create Account</a></li>
+                <li><a href="Login.html">Login</a></li>
+            </ul>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="Product.html">Product</a></li>
+                <li><a href="Contact.html">Contact</a></li>
+                <li><a href="Size.html">Size Guide</a></li>
+            </ul>
+        </nav> 
+    </header>
+</body>
+<footer>
+    <div>
+        <h3>How to contact us?</h3>
+        <ul>
+            <li><img src="../img/telephone.png" alt="telephone logo" width="100">44909999</li>
+            <li><img src="../img/whatsapp.png" alt="whatsapp image" width="100"><a href="https://www.whatsapp.com">+54 9999 99-9999</a></img></li>
+            <li><img src="../img/email.png" alt="email logo" width="100">example@gmail.com</li>
+        </ul>
+    </div>
+    <div>
+        <h3>Follow us!</h3>
+        <ul>
+            <li><a href="https://www.instagram.com"><img src="../img/instagram.png" alt="instagram logo" width="100"></a></li>
+            <li><a href="https://www.facebook.com"><img src="../img/facebook.png" alt="facebook logo" width="100"></a></li>
+        </ul>
+    </div>
+    <div>
+        <h3>Payment Methods</h3>
+        <img src="../img/visa@2x.png" alt="visa logo" width="60">
+        <img src="../img/mastercard@2x.png" alt="mastercard logo" width="60">
+        <img src="../img/mercadopago@2x.png" alt="mercadopago logo" width="60">
+        <img src="../img/paypal@2x.png" alt="paypal logo" width="60">
+    </div>
+    <div>
+        <h3>Shipphing Methods</h3>
+        <img src="../img/2682@2x.png" alt="correoArg logo" width="60">
+        <img src="../img/3336@2x.png" alt="andreaniLogo" width="60">
+    </div>
+    <div>
+        <h5>Copyright CloudPads - 2024. All rights reserved.</h5>
+</footer>
+</html>
